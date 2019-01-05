ffc5d4c38d98717e97fd1ea91aaebc4da536fef8
Alexander Barbaranelli <barbaranelli.a@gmail.com>
2019-01-05 16:06:46 +0100

    Reload on wrong ingredient

--- script.js
+++ script.js
@@ -6,21 +6,61 @@ $( document ).ready(function() {
     $('#homeImage').fadeOut();
     $('.ingredients').delay(500).fadeIn();
     $('#container').delay(500).fadeIn();
+    $('#shakeButton').delay(500).fadeIn();
+    $('#pourButton').delay(500).fadeIn();
+    $('#serveButton').delay(500).fadeIn();
   });
 
   $('#topCenter').click(function() {
     location.reload();
   });
 
+  var whiskeyAdded = false;
+  var lemonAdded = false;
+  var angosturaAdded = false;
+  var eggAdded = false;
+  var iceAdded = false;
+  var syrupAdded = false;
+
   var totalHeight = 0;
   var mixedColor;
 
+  $('#yesButton').click(function(){
+    console.log("YES BUTTON");
+    $(this).data('clicked', true);
+  });
+
   $("#whiskeyButton").click(function() {
-    $('.pour').css("background-color", "orange");
-    $('#liquid').css("background-color", "orange");
-    $('.pour').delay(500).animate({height: '360px'}, 1500).delay(2000).slideUp(500);
-    $('#liquid').delay(1900).animate({height: parseInt(totalHeight) + 50 + "px"}, 2500);
-    totalHeight = totalHeight + 50;
+    if(whiskeyAdded == true){
+      $('#container').fadeOut();
+      $('#popup').delay(500).fadeIn();
+      $('#yesButton').delay(500).fadeIn();
+      $('#noButton').delay(500).fadeIn();
+
+      $('#noButton').click(function(){
+        $('#popup').fadeOut();
+        $('#yesButton').fadeOut();
+        $('#noButton').fadeOut();
+        $('#container').delay(500).fadeIn();
+      });
+
+      $('#yesButton').click(function(){
+        $('#popup').fadeOut();
+        $('#yesButton').fadeOut();
+        $('#noButton').fadeOut();
+        $('#disgusting').delay(500).fadeIn();
+        setTimeout(location.reload.bind(location), 2000);
+      });
+
+    }
+    else{
+      $('.pour').css("background-color", "orange");
+      $('#liquid').css("background-color", "orange");
+      $('.pour').delay(500).animate({height: '360px'}, 1500).delay(2000).slideUp(500);
+      $('#liquid').delay(1900).animate({height: parseInt(totalHeight) + 50 + "px"}, 2500);
+      totalHeight = totalHeight + 50;
+      whiskeyAdded = true;
+    }
   });
 
   $("#lemonButton").click(function() {
@@ -57,6 +97,10 @@ $( document ).ready(function() {
     $('.pour5').delay(500).animate({height: '360px'}, 1500).delay(2000).slideUp(500);
     $('#liquid').delay(1900).animate({height: parseInt(totalHeight) + 25 + "px"}, 2500);
     totalHeight = totalHeight + 50;
+  });
+
+  $("#shakeButton").click(function() {
+    $('#container').effect("shake");
   });
 
 });
